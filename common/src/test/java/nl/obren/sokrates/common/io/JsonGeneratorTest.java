a83957eb9705f3e9a4c1def4c9f297d105acc3ae
Dmitry Litosh <dmlitosh@gmail.com>
2020-01-04 05:58:15 +0100

    fixed the test not running on Windows (different line separators)

--- common/src/test/java/nl/obren/sokrates/common/io/JsonGeneratorTest.java
+++ common/src/test/java/nl/obren/sokrates/common/io/JsonGeneratorTest.java
@@ -4,27 +4,41 @@
 
 package nl.obren.sokrates.common.io;
 
-import org.junit.Test;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.condition.OS.LINUX;
+import static org.junit.jupiter.api.condition.OS.MAC;
+import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
 public class JsonGeneratorTest {
 
-    @Test
-    public void testGenerateJson() throws Exception {
+    private String generateJson() throws JsonProcessingException {
         class A {
-            private String a = "a";
-
-            public String getA() {
-                return a;
-            }
-
-            public void setA(String a) {
-                this.a = a;
-            }
+        private String a = "a";
+        public String getA() {
+            return a;
         }
+        public void setA(String a) {
+            this.a = a;
+        }
+    }
+        return new JsonGenerator().generate(new A());
+}
 
-        String json = new JsonGenerator().generate(new A());
+    @Test
+    @EnabledOnOs({LINUX, MAC})
+    public void testGenerateJsonOnLinuxOrMac() throws Exception {
+        String json = generateJson();
         assertEquals(json, "{\n  \"a\" : \"a\"\n}");
     }
+
+    @Test
+    @EnabledOnOs(WINDOWS)
+    public void testGenerateJsonOnWindows() throws Exception {
+        String json = generateJson();
+        assertEquals(json, "{\r\n  \"a\" : \"a\"\r\n}");
+    }
+
 }
