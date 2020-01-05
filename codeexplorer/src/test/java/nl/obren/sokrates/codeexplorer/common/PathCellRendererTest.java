452225772ddfb336c5bf03b3cec167bea5ef9032
Dmitry Litosh <dmlitosh@gmail.com>
2020-01-05 17:13:06 +0100

    getDefault().getSeparator() instead of mac "/"

--- codeexplorer/src/test/java/nl/obren/sokrates/codeexplorer/common/PathCellRendererTest.java
+++ codeexplorer/src/test/java/nl/obren/sokrates/codeexplorer/common/PathCellRendererTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import java.io.File;
 
+import static java.nio.file.FileSystems.getDefault;
 import static junit.framework.TestCase.assertEquals;
 
 public class PathCellRendererTest {
@@ -15,8 +16,8 @@ public class PathCellRendererTest {
     public void getPathPrefix() throws Exception {
         PathCellRenderer renderer = new PathCellRenderer();
 
-        assertEquals(renderer.getPathPrefix(new File("/root/a/b/c/A.java")), "/root/a/b/c/");
-        assertEquals(renderer.getPathPrefix(new File("a/A.java")), "a/");
+        assertEquals(renderer.getPathPrefix(new File("/root/a/b/c/A.java")), getDefault().getSeparator() + "root" + getDefault().getSeparator() + "a" + getDefault().getSeparator() + "b" + getDefault().getSeparator() + "c" + getDefault().getSeparator());
+        assertEquals(renderer.getPathPrefix(new File("a/A.java")), "a" + getDefault().getSeparator());
         assertEquals(renderer.getPathPrefix(new File("A.java")), "");
     }
 
