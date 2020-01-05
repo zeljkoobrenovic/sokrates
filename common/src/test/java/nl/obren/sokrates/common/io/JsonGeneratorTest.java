58cf46008624f15c6e0fa652b60f34241f0f7c92
Dmitry Litosh <dmlitosh@gmail.com>
2020-01-05 10:14:38 +0100

    Revert "fixed the test not running on Windows (different line separators)"
    This reverts commit a83957eb

--- common/src/test/java/nl/obren/sokrates/common/io/JsonGeneratorTest.java
+++ common/src/test/java/nl/obren/sokrates/common/io/JsonGeneratorTest.java
@@ -4,41 +4,27 @@
 
 package nl.obren.sokrates.common.io;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.condition.OS.LINUX;
-import static org.junit.jupiter.api.condition.OS.MAC;
-import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
 public class JsonGeneratorTest {
 
-    private String generateJson() throws JsonProcessingException {
-        class A {
-        private String a = "a";
-        public String getA() {
-            return a;
-        }
-        public void setA(String a) {
-            this.a = a;
-        }
-    }
-        return new JsonGenerator().generate(new A());
-}
-
     @Test
-    @EnabledOnOs({LINUX, MAC})
-    public void testGenerateJsonOnLinuxOrMac() throws Exception {
-        String json = generateJson();
+    public void testGenerateJson() throws Exception {
+        class A {
+            private String a = "a";
+
+            public String getA() {
+                return a;
+            }
+
+            public void setA(String a) {
+                this.a = a;
+            }
+        }
+
+        String json = new JsonGenerator().generate(new A());
         assertEquals(json, "{\n  \"a\" : \"a\"\n}");
     }
-
-    @Test
-    @EnabledOnOs(WINDOWS)
-    public void testGenerateJsonOnWindows() throws Exception {
-        String json = generateJson();
-        assertEquals(json, "{\r\n  \"a\" : \"a\"\r\n}");
-    }
-
 }
