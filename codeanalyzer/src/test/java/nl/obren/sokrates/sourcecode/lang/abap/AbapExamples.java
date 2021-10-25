3fe1a5133c61cfba9449890075ace515819a5036
emilholmegaard <emilholmegaard@gmail.com>
2021-10-25 05:26:22 +0200

    cleaned content to match cleaning for duplication

--- codeanalyzer/src/test/java/nl/obren/sokrates/sourcecode/lang/abap/AbapExamples.java
+++ codeanalyzer/src/test/java/nl/obren/sokrates/sourcecode/lang/abap/AbapExamples.java
@@ -140,60 +140,60 @@ class AbapExamples {
         "  ENDMETHOD.\n"+
         "ENDCLASS.";
         public static final String CLEANED_FOR_DUPLICATION ="CLASS zcl_oo_tutorial_3 DEFINITION\n"+
-        "  PUBLIC\n"+
-        "  FINAL\n"+
-        "  CREATE PUBLIC .\n"+
-        "  PUBLIC SECTION.\n"+
-        "    INTERFACES if_serializable_object.\n"+
-        "    DATA flight TYPE /dmo/flight.\n"+
-        "    METHODS constructor\n"+
-        "      IMPORTING\n"+
-        "        !carrier_id    TYPE /dmo/carrier_id\n"+
-        "        !connection_id TYPE /dmo/connection_id\n"+
-        "        !flight_date   TYPE /dmo/flight_date.\n"+
-        "    METHODS get_flight_details\n"+
-        "      RETURNING VALUE(flight) TYPE /dmo/flight .\n"+
-        "    METHODS calculate_flight_price\n"+
-        "      EXPORTING\n"+
-        "        !price         TYPE /dmo/flight_price\n"+
-        "        !currency_code TYPE /dmo/currency_code.\n"+
-        "  PROTECTED SECTION.\n"+
-        " PRIVATE SECTION.\n"+
+        "PUBLIC\n"+
+        "FINAL\n"+
+        "CREATE PUBLIC .\n"+
+        "PUBLIC SECTION.\n"+
+        "INTERFACES if_serializable_object.\n"+
+        "DATA flight TYPE /dmo/flight.\n"+
+        "METHODS constructor\n"+
+        "IMPORTING\n"+
+        "!carrier_id TYPE /dmo/carrier_id\n"+
+        "!connection_id TYPE /dmo/connection_id\n"+
+        "!flight_date TYPE /dmo/flight_date.\n"+
+        "METHODS get_flight_details\n"+
+        "RETURNING VALUE(flight) TYPE /dmo/flight .\n"+
+        "METHODS calculate_flight_price\n"+
+        "EXPORTING\n"+
+        "!price TYPE /dmo/flight_price\n"+
+        "!currency_code TYPE /dmo/currency_code.\n"+
+        "PROTECTED SECTION.\n"+
+        "PRIVATE SECTION.\n"+
         "ENDCLASS.\n"+
         "CLASS zcl_oo_tutorial_3 IMPLEMENTATION.\n"+
-        "  METHOD calculate_flight_price.\n"+
-        "    price = me->flight-price.\n"+
-        "    currency_code = me->flight-currency_code.\n"+
-        "    CASE me->flight-plane_type_id.\n"+
-        "      WHEN \'747-400\'.\n"+
-        "        price = price + 40.\n"+
-        "      WHEN \'A310-300\'.\n"+
-        "        price = price + 25.\n"+
-        "      WHEN OTHERS.\n"+
-        "        price = price + 10.\n"+
-        "    ENDCASE.\n"+
-        "  ENDMETHOD.\n"+
-        "  METHOD calculate_flight_price2.\n"+
-        "    price = me->flight-price.\n"+
-        "    currency_code = me->flight-currency_code.\n"+
-        "    CASE me->flight-plane_type_id.\n"+
-        "      WHEN \'747-400\'.\n"+
-        "        price = price + 40.\n"+
-        "      WHEN \'A310-300\'.\n"+
-        "        price = price + 25.\n"+
-        "      WHEN OTHERS.\n"+
-        "        price = price + 10.\n"+
-        "    ENDCASE.\n"+
-        "  ENDMETHOD.\n"+
-        "  METHOD constructor.\n"+
-        "    SELECT SINGLE * FROM /dmo/flight\n"+
-        "      WHERE carrier_id = @carrier_id\n"+
-        "        AND connection_id = @connection_id\n"+
-        "        AND flight_date = @flight_date\n"+
-        "       INTO @flight.\n"+
-        "  ENDMETHOD.\n"+
-        "  METHOD get_flight_details.\n"+
-        "    flight = me->flight.\n"+
-        "  ENDMETHOD.\n"+
+        "METHOD calculate_flight_price.\n"+
+        "price = me->flight-price.\n"+
+        "currency_code = me->flight-currency_code.\n"+
+        "CASE me->flight-plane_type_id.\n"+
+        "WHEN \'747-400\'.\n"+
+        "price = price + 40.\n"+
+        "WHEN \'A310-300\'.\n"+
+        "price = price + 25.\n"+
+        "WHEN OTHERS.\n"+
+        "price = price + 10.\n"+
+        "ENDCASE.\n"+
+        "ENDMETHOD.\n"+
+        "METHOD calculate_flight_price2.\n"+
+        "price = me->flight-price.\n"+
+        "currency_code = me->flight-currency_code.\n"+
+        "CASE me->flight-plane_type_id.\n"+
+        "WHEN \'747-400\'.\n"+
+        "price = price + 40.\n"+
+        "WHEN \'A310-300\'.\n"+
+        "price = price + 25.\n"+
+        "WHEN OTHERS.\n"+
+        "price = price + 10.\n"+
+        "ENDCASE.\n"+
+        "ENDMETHOD.\n"+
+        "METHOD constructor.\n"+
+        "SELECT SINGLE * FROM /dmo/flight\n"+
+        "WHERE carrier_id = @carrier_id\n"+
+        "AND connection_id = @connection_id\n"+
+        "AND flight_date = @flight_date\n"+
+        "INTO @flight.\n"+
+        "ENDMETHOD.\n"+
+        "METHOD get_flight_details.\n"+
+        "flight = me->flight.\n"+
+        "ENDMETHOD.\n"+
         "ENDCLASS.";      
 }
